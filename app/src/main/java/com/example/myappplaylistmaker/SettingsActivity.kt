f8395d7c512dbc83de5ed9cb982c736c1ec1b2d2
Ekaterina <ekaterinalife92@yandex.ru>
2024-08-17 16:14:58 +0300

    [MyAppPlaylistMaker] test commit

--- app/src/main/java/com/example/myappplaylistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/myappplaylistmaker/SettingsActivity.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        setContentView(R.layout.activity_settings_light)
+        setContentView(R.layout.activity_settings)
         findViewById<Button>(R.id.arrow).setOnClickListener {
             finish()
         }
