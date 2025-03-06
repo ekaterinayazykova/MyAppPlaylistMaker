e3fb468740b14cd7e63d87d4d99664782cf10d94
Ekaterina <ekaterinalife92@yandex.ru>
2025-03-06 12:37:50 +0300

    Sprint-22: problems with layout

--- app/src/main/java/com/example/myappplaylistmaker/presentation/ui/library/CreatePlaylistFragment.kt
+++ app/src/main/java/com/example/myappplaylistmaker/presentation/ui/library/CreatePlaylistFragment.kt
@@ -1,17 +1,54 @@
 package com.example.myappplaylistmaker.presentation.ui.library
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity.INPUT_METHOD_SERVICE
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.myappplaylistmaker.R
 import com.example.myappplaylistmaker.databinding.FragmentCreationBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
+import java.io.FileOutputStream
 
-class CreatePlaylistFragment: Fragment() {
+class CreatePlaylistFragment : Fragment() {
 
     private var _binding: FragmentCreationBinding? = null
     private val binding get() = _binding!!
+    private var nameInput: String = ""
+    private var descriptionInput: String = ""
+    private var uploadedCover: Uri? = null
+
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        if (uri != null) {
+            uploadedCover = uri
+            binding.addPhoto.setImageURI(uri)
+            saveImageToPrivateStorage(uri)
+        } else {
+            Log.d("PhotoPicker", "No media selected")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,12 +59,37 @@ class CreatePlaylistFragment: Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.arrow.setOnClickListener{
+        updateButtonState()
+        editName()
+        editDescription()
+
+        binding.arrow.setOnClickListener {
+            confirmDialog()
+        }
+
+        binding.addPhoto.setOnClickListener {
+            pickMedia.launch(
+                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+            )
+        }
+
+        binding.buttonCreate.setOnClickListener {
+            showSnackbar()
             findNavController().navigateUp()
         }
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object: OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    confirmDialog()
+                }
+            }
+        )
     }
 
     override fun onDestroyView() {
@@ -35,4 +97,97 @@ class CreatePlaylistFragment: Fragment() {
         _binding = null
     }
 
+    override fun onResume() {
+        super.onResume()
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+    }
+
+    private fun editName() {
+        binding.editName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                nameInput = s.toString()
+                updateButtonState()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+    private fun editDescription() {
+        binding.editDescription.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                descriptionInput = s.toString()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun updateButtonState() {
+        binding.buttonCreate.isEnabled = !(nameInput.isBlank() || nameInput.isEmpty())
+    }
+
+    private fun toggleKeyboard(view: View, show: Boolean) {
+        val imm = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        if (show) {
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+        } else {
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun confirmDialog() {
+        if (nameInput.isNotEmpty() || descriptionInput.isNotEmpty() ||
+            nameInput.isNotBlank() || descriptionInput.isNotBlank() ||
+            uploadedCover != null)  {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.saveConfirmTitle)
+                .setMessage(R.string.saveConfirmMessage)
+                .setNeutralButton(R.string.cancel) { dialog, witch ->
+
+                }.setPositiveButton(R.string.finish) { dialog, witch ->
+//                    TODO()
+                    findNavController().navigateUp()
+                }.show()
+        } else {
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun saveImageToPrivateStorage(uri: Uri) {
+        val filePath = File(requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), getString(R.string.myAlbum))
+        if (!filePath.exists()) {
+            filePath.mkdirs()
+        }
+        val file = File(filePath, "first_cover.jpg")
+        val inputStream = requireActivity().contentResolver.openInputStream(uri)
+        val outputStream = FileOutputStream(file)
+        BitmapFactory
+            .decodeStream(inputStream)
+            .compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
+        outputStream.close()
+        inputStream?.close()
+    }
+
+    private fun showSnackbar() {
+        val playlistName = binding.editName.text.toString().trim()
+        if (playlistName.isNotEmpty()) {
+            val snackbar =
+                Snackbar.make(binding.root, "Плейлист $playlistName создан", Snackbar.LENGTH_LONG)
+            val textView =
+                snackbar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+            val typeface = ResourcesCompat.getFont(requireContext(), R.font.ys_display_regular)
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
+            textView.setTypeface(typeface)
+            val params = textView.layoutParams as LinearLayout.LayoutParams
+            params.width = LinearLayout.LayoutParams.MATCH_PARENT
+            textView.layoutParams = params
+            textView.gravity = Gravity.CENTER
+            snackbar.show()
+        }
+    }
 }
